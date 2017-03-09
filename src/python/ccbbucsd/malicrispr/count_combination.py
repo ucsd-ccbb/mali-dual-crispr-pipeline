2d0aff35aacdbec2e30eb1120888b52e24308899
Amanda Birmingham <abirmingham@ucsd.edu>
2017-03-08 16:27:27 -0800

    Reformatted code

--- src/python/ccbbucsd/malicrispr/count_combination.py
+++ src/python/ccbbucsd/malicrispr/count_combination.py
@@ -27,40 +27,42 @@ def group_lane_and_set_files(filepaths):
 
 def combine_count_files(counts_fp_for_dataset, run_prefix):
     combined_df = None
-    
+
     for curr_counts_fp in counts_fp_for_dataset:
         count_header, curr_counts_df = get_counts_df(curr_counts_fp, run_prefix)
-        
+
         if combined_df is None:
             combined_df = curr_counts_df
         else:
             combined_df[count_header] = curr_counts_df[count_header]
-    
+
     return combined_df
 
 
-def write_collapsed_count_files(input_dir, output_dir, curr_run_prefix, counts_run_prefix, counts_suffix, counts_collapsed_file_suffix):
+def write_collapsed_count_files(input_dir, output_dir, curr_run_prefix, counts_run_prefix, counts_suffix,
+                                counts_collapsed_file_suffix):
     counts_fps_for_dataset = get_filepaths_by_prefix_and_suffix(input_dir, counts_run_prefix, counts_suffix)
     fps_by_sample = group_lane_and_set_files(counts_fps_for_dataset)
-    
+
     for curr_sample, curr_fps in fps_by_sample.items():
         stripped_sample = strip_run_prefix(curr_sample, counts_run_prefix)
-        output_fp = build_multipart_fp(output_dir, [curr_run_prefix, stripped_sample, counts_collapsed_file_suffix]) 
-        combined_df = None        
-        
+        output_fp = build_multipart_fp(output_dir, [curr_run_prefix, stripped_sample, counts_collapsed_file_suffix])
+        combined_df = None
+
         for curr_fp in curr_fps:
             count_header, curr_counts_df = get_counts_df(curr_fp, counts_run_prefix)
-        
+
             if combined_df is None:
                 combined_df = curr_counts_df
-                combined_df.rename(columns = {count_header:stripped_sample}, inplace = True) 
+                combined_df.rename(columns={count_header: stripped_sample}, inplace=True)
             else:
                 combined_df[stripped_sample] = combined_df[stripped_sample] + curr_counts_df[count_header]
-    
-        combined_df.to_csv(output_fp, sep="\t", index=False)    
+
+        combined_df.to_csv(output_fp, sep="\t", index=False)
 
 
-def write_combined_count_file(input_dir, output_dir, curr_run_prefix, counts_run_prefix, counts_suffix, combined_suffix):
+def write_combined_count_file(input_dir, output_dir, curr_run_prefix, counts_run_prefix, counts_suffix,
+                              combined_suffix):
     output_fp = build_multipart_fp(output_dir, [curr_run_prefix, combined_suffix])
     counts_fps_for_run = get_filepaths_by_prefix_and_suffix(input_dir, counts_run_prefix, counts_suffix)
     combined_df = combine_count_files(counts_fps_for_run, curr_run_prefix)
--- src/python/ccbbucsd/malicrispr/count_plots.py
+++ src/python/ccbbucsd/malicrispr/count_plots.py
@@ -5,7 +5,8 @@ import pandas
 
 # ccbb libraries
 from ccbbucsd.utilities.analysis_run_prefixes import strip_run_prefix
-from ccbbucsd.utilities.files_and_paths import build_multipart_fp, get_file_name_pieces, get_filepaths_by_prefix_and_suffix
+from ccbbucsd.utilities.files_and_paths import build_multipart_fp, get_file_name_pieces, \
+    get_filepaths_by_prefix_and_suffix
 
 # project-specific libraries
 from ccbbucsd.malicrispr.count_files_and_dataframes import get_counts_df
@@ -30,10 +31,10 @@ def make_log2_series(input_series, pseudocount_val):
     # note that .reset_index(drop=True) is necessary as matplotlib boxplot function (perhaps among others)
     # throws an error if the input series doesn't include an item with index 0--which can be the case if
     # that first item was NaN and was dropped, and series wasn't reindexed.
-    
+
 
 def show_and_save_histogram(output_fp, title, count_data):
-    matplotlib.pyplot.figure(figsize=(20,20))
+    matplotlib.pyplot.figure(figsize=(20, 20))
     matplotlib.pyplot.hist(count_data)
     matplotlib.pyplot.title(title)
     matplotlib.pyplot.xlabel("log2(raw counts)")
@@ -43,10 +44,10 @@ def show_and_save_histogram(output_fp, title, count_data):
 
 
 def show_and_save_boxplot(output_fp, title, samples_names, samples_data, rotation_val=0):
-    fig = matplotlib.pyplot.figure(1, figsize=(20,20))
+    fig = matplotlib.pyplot.figure(1, figsize=(20, 20))
     ax = fig.add_subplot(111)
     bp = ax.boxplot(samples_data)
-    ax.set_xticklabels(samples_names, rotation=rotation_val)   
+    ax.set_xticklabels(samples_names, rotation=rotation_val)
     ax.set_xlabel("samples")
     ax.set_ylabel("log2(raw counts)")
 
@@ -57,26 +58,27 @@ def show_and_save_boxplot(output_fp, title, samples_names, samples_data, rotatio
 
 def plot_raw_counts(input_dir, input_run_prefix, counts_suffix, output_dir, output_run_prefix, boxplot_suffix):
     counts_fps_for_run = get_filepaths_by_prefix_and_suffix(input_dir, input_run_prefix, counts_suffix)
-    
+
     for curr_counts_fp in counts_fps_for_run:
         _, curr_sample, _ = get_file_name_pieces(curr_counts_fp)
         stripped_sample = strip_run_prefix(curr_sample, input_run_prefix)
         count_header, curr_counts_df = get_counts_df(curr_counts_fp, input_run_prefix)
-        curr_counts_df.rename(columns={count_header:stripped_sample}, inplace=True)
+        curr_counts_df.rename(columns={count_header: stripped_sample}, inplace=True)
         count_header = stripped_sample
         log2_series = make_log2_series(curr_counts_df[count_header], DEFAULT_PSEUDOCOUNT)
-        
+
         title = " ".join([input_run_prefix, count_header, "with pseudocount", str(DEFAULT_PSEUDOCOUNT)])
         output_fp_prefix = build_multipart_fp(output_dir, [count_header, input_run_prefix])
-        
+
         boxplot_fp = output_fp_prefix + "_" + boxplot_suffix
         show_and_save_boxplot(boxplot_fp, title, [count_header], log2_series)
-        
+
         hist_fp = output_fp_prefix + "_" + "hist.png"
         show_and_save_histogram(hist_fp, title, log2_series)
-        
-        
-def plot_combined_raw_counts(input_dir, input_run_prefix, combined_suffix, output_dir, output_run_prefix, boxplot_suffix):
+
+
+def plot_combined_raw_counts(input_dir, input_run_prefix, combined_suffix, output_dir, output_run_prefix,
+                             boxplot_suffix):
     output_fp = build_multipart_fp(output_dir, [output_run_prefix, boxplot_suffix])
     combined_counts_fp = build_multipart_fp(input_dir, [input_run_prefix, combined_suffix])
     combined_counts_df = pandas.read_table(combined_counts_fp)
@@ -85,6 +87,6 @@ def plot_combined_raw_counts(input_dir, input_run_prefix, combined_suffix, outpu
     for curr_name in samples_names:
         log2_series = make_log2_series(combined_counts_df[curr_name], DEFAULT_PSEUDOCOUNT)
         samples_data.append(log2_series.tolist())
-        
+
     title = " ".join([input_run_prefix, "all samples", "with pseudocount", str(DEFAULT_PSEUDOCOUNT)])
     show_and_save_boxplot(output_fp, title, samples_names, samples_data, 90)
