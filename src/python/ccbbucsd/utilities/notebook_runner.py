c35200cbfb3971e5ae8dea08ad8b9602df68a268
Amanda Birmingham <abirmingham@ucsd.edu>
2017-03-22 13:49:34 -0700

    Added get_params_from_nb_file to notebook_runner.  Wrote tests for notebook_runner.

--- src/python/ccbbucsd/utilities/notebook_runner.py
+++ src/python/ccbbucsd/utilities/notebook_runner.py
@@ -1,6 +1,7 @@
 # standard libraries
 import os
 
+
 # third-party libraries
 import nbformat
 import nbparameterise
@@ -8,7 +9,7 @@ from nbconvert import HTMLExporter
 from nbconvert.preprocessors import ExecutePreprocessor
 
 # ccbb libraries
-from ccbbucsd.utilities.files_and_paths import get_file_name_pieces, make_file_path
+import ccbbucsd.utilities.files_and_paths as ns_files
 
 __author__ = "Amanda Birmingham"
 __maintainer__ = "Amanda Birmingham"
@@ -22,6 +23,11 @@ def read_in_notebook(notebook_fp):
     return nb
 
 
+def get_params_from_nb_file(notebook_fp):
+    nb = read_in_notebook(notebook_fp)
+    return nbparameterise.extract_parameters(nb)
+
+
 def set_parameters(nb, params_dict):
     orig_parameters = nbparameterise.extract_parameters(nb)
     params = nbparameterise.parameter_values(orig_parameters, **params_dict)
@@ -30,7 +36,7 @@ def set_parameters(nb, params_dict):
 
 
 # modified from https://nbconvert.readthedocs.io/en/latest/execute_api.html
-def execute_notebook(notebook_filename, notebook_filename_out, params_dict, run_path="", timeout=6000000):
+def execute_notebook(notebook_filename, params_dict, notebook_filename_out, run_path="", timeout=6000000):
     notebook_fp = os.path.join(run_path, notebook_filename)
     nb = read_in_notebook(notebook_fp)
     new_nb = set_parameters(nb, params_dict)
@@ -52,7 +58,9 @@ def export_notebook_to_html(notebook_fp, output_dir):
     nb = read_in_notebook(notebook_fp)
     html_exporter = HTMLExporter()
     body, resources = html_exporter.from_notebook_node(nb)
-    _, notebook_name, _ = get_file_name_pieces(notebook_fp)
-    out_fp = make_file_path(output_dir, notebook_name, ".html")
+    _, notebook_name, _ = ns_files.get_file_name_pieces(notebook_fp)
+    out_fp = ns_files.make_file_path(output_dir, notebook_name, ".html")
     with open(out_fp, "w", encoding="utf8") as f:
         f.write(body)
+
+    return out_fp
