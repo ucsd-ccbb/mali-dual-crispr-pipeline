76230d6a2ddc6ff63c848d063ec81609d155479f
Amanda Birmingham <abirmingham@ucsd.edu>
2017-03-23 13:22:06 -0700

    Rewrote so this is currently just a wrapper for set_up_data_subdirs_and_get_machine_configs in dual_crispr_pipeliner module.  No tests because that method is tested in the tests for that module.

--- src/python/set_up_mali_pipeline.py
+++ src/python/set_up_mali_pipeline.py
@@ -3,7 +3,7 @@ import dual_crispr_pipeliner as ns_dcpipe
 
 
 def main():
-    ns_dcpipe.generate_dirs_dict(fastq_set_name="")
+    ns_dcpipe.set_up_data_subdirs_and_get_machine_configs()
 
 if __name__ == '__main__':
     main()
